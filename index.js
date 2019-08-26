4f17548966be59b8f12d0d1aef5f1dfdb0a23376
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-26 03:55:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ function theBeatlesPlay(musicians, instruments) {
   
   for(let i = 0; i < musicians.length; i++) {
    var bandMember = `${musicians[i]} plays ${instruments[i]}`
+   /*
+   using template literals, musicians[i] allows for selecting the element from the array musicians at index [i], which starts at 0 since we set our counter to 0, and each loop adds to the counter so first time is musicians[0], then musicians[1] then musicians[2], etc, until condition is met which is when the the value of i is a higher # then the value of the length of the arrays
+   */
+   
    bandInfo.push(bandMember)
+   /* 
+   array.push() method allows for adding an element to the end of an array
+   */
   }
   return bandInfo
 }
+
+function johnLennonFacts()
