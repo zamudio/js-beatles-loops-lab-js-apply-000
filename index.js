59ff0cb9a3f90b3ce04cfb98839569782cc1060a
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-26 03:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musiciansInfo = []
+  
+  for(let i = 0; i < musicians.length; i++) {
+    musiciansInfo += musiciansInfo + `${musicians[i]} plays ${instruments[i]}`
+  }
+  return musiciansInfo
+}
