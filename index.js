bd5a486d68b1327ef09c176921bb415e2b341b53
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-26 04:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num) {
   var beatlesLove = []
   do {
-    var beatlesLove.push("I love the Beatles!")
+    beatlesLove.push("I love the Beatles!")
     num++
   } while (num < 15)
   return beatlesLove
