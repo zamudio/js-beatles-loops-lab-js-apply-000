a025b2e4742f47546d02b2e4092296adc1b9d4c7
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-26 03:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function theBeatlesPlay(musicians, instruments) {
   var musiciansInfo = []
   
   for(let i = 0; i < musicians.length; i++) {
-    musiciansInfo += musiciansInfo + `${musicians[i]} plays ${instruments[i]}`
+   var bandMember = `${musicians[i]} plays ${instruments[i]}`
+   musiciansInfo = bandMember
   }
   return musiciansInfo
 }
